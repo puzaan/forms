d616a52f98ede6fca39423d0c4c5ab4cc38aed09
Puzaan Xhres <41267289+puzaan@users.noreply.github.com>
2021-07-16 12:32:06 +0545

    connected with mongoose db

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
-const express = require('express');
+import express from 'express'
+import connectDB from './config/db.js';
+import cors from 'cors'
+import dotenv from 'dotenv'
+import colors from 'colors'
 
 const app = express();
+dotenv.config();
+app.use(cors());
 
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('API server is working');
 })
 
-app.listen(5000, console.log('Server is running on port 5000'))
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log(`Server is running on port ${PORT}`.yellow.bold))
