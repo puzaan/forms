060df2298a6a2bf14677d68d791a135eb260fed1
Puzaan Xhres <41267289+puzaan@users.noreply.github.com>
2021-07-16 16:25:57 +0545

    added error handler in api

--- backend/controllers/formController.js
+++
@@ -1,9 +0,0 @@
-import Form from "../models/formModel.js";
-import asyncHandler from 'express-async-handler'
-
-
-const getForm = asyncHandler(async (req, res)=> {
-    let 
-})
-
-
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import formRouts from './routs/formRouts.js'
 import userRouts from './routs/userRouts.js'
+import { notFound, errorHandler } from './middlewares/errorMiddleware.js';
 
 const app = express();
 dotenv.config();
@@ -18,8 +19,16 @@ app.get('/', (req, res) => {
     res.send('API server is working');
 })
 app.use('/api/customerForm', formRouts);
+app.use('/api/users', userRouts);
 
-app.use('/api/users', userRouts)
+
+app.use(notFound);
+app.use(errorHandler);
+
+
+
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(5000, console.log(`Server is running on port ${PORT}`.yellow.bold))
